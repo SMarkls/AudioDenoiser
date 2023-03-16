50bd3697a3f2d7e85d9b6bd837ee128b39011187
markls <isxakov.mark@gmail.com>
2023-03-16 21:31:55 +0500

    Начальный коммит.

---
+++ ApplicationDenoiser.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using KoshkinSoft.DenoiserNative;
+
+namespace ApplicationDenoiser.Console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var denoiser = new Denoiser())
+                denoiser.Denoise(args[0]);
+        }
+    }
+}
